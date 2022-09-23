19d5117db50cc878baf895fc9c5ac050532b40c7
Muhammad Aneek Rahman <reflection.active@gmail.com>
2022-09-24 01:30:29 +0600

    - Finish Events gallery

--- client/src/pages/GenericPages/ViewEventBody.tsx
+++ client/src/pages/GenericPages/ViewEventBody.tsx
@@ -1,8 +1,11 @@
-import { Box, Center, Spinner, Text } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import { Box, Center, Spinner } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { FirestoreRequests, Event } from "../../components/constansts";
 import styles from "../../styles/GenericPages/ViewEventBody.module.scss";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 export default function ViewEventBody() {
   const [thisEvent, setShisEvent] = useState<Event | undefined>(undefined);
@@ -43,8 +46,44 @@ export default function ViewEventBody() {
 
   return (
     <div className={styles.ViewEventBody}>
+      <i>Event details:</i>
       <h3>{thisEvent?.title}</h3>
       <p>{new Date(thisEvent?.timeCreated).toLocaleDateString()}</p>
+      <CarouselGallery imageURLs={thisEvent.imageURLs} />
+    </div>
+  );
+}
+
+function CarouselGallery({ imageURLs }: { imageURLs: string[] }) {
+  return (
+    <div className={styles.CarouselWrapper}>
+      <Carousel
+        showArrows={true}
+        renderArrowPrev={(e) => {
+          return (
+            <FaAngleLeft
+              className={styles.LeftArrow}
+              color="white"
+              onClick={e}
+            />
+          );
+        }}
+        renderArrowNext={(e) => {
+          return (
+            <FaAngleRight
+              className={styles.RightArrow}
+              color="white"
+              onClick={e}
+            />
+          );
+        }}
+      >
+        {imageURLs.map((url) => (
+          <div className={styles.CarouselItem}>
+            <img src={url} alt="" />
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 }
