c24a9673610b65b18fc0ce0c5368315ed02e1c22
Muhammad Aneek Rahman <reflection.active@gmail.com>
2022-09-24 20:39:42 +0600

    - Create MyTexts to group all the institute specific texts - Make Google sign in exclusive for one account on the Clientside

---
+++ client/src/components/texts.tsx
@@ -0,0 +1,3 @@
+export default class MyTexts {
+  static SERVER_ACCOUNT_UID: string = "IN3K0LR0iIUDtr0sfLC2Y2Tpiac2";
+}
--- client/src/pages/AdminPanel/DashboardPage.tsx
+++ client/src/pages/AdminPanel/DashboardPage.tsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
   User,
 } from "firebase/auth";
-import { Button, useToast } from "@chakra-ui/react";
+import { Box, Button, Center, Text, useToast } from "@chakra-ui/react";
+import { FcGoogle } from "react-icons/fc";
+import MyTexts from "../../components/texts";
 
 export default function DashboardPage() {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
-  const toast = useToast();
 
   useEffect(() => {
     // Listen to auth state changed
@@ -21,22 +22,90 @@ export default function DashboardPage() {
   return (
     <div>
       {currentUser ? (
-        <Button>Logout</Button>
+        <div>
+          {/* Check if account has access */}
+          {currentUser.uid !== MyTexts.SERVER_ACCOUNT_UID ? (
+            <AccountNoAccess />
+          ) : (
+            <DashboardSideBox currentUser={currentUser} />
+          )}
+        </div>
       ) : (
+        <LoginBox />
+      )}
+    </div>
+  );
+}
+
+function DashboardSideBox({ currentUser }: { currentUser: User }) {
+  return (
+    <Box p={20}>
+      <Text>Welcome: {currentUser.displayName}. This is the dashboard</Text>
+      <Button onClick={(e) => getAuth().signOut()}>Logout</Button>
+    </Box>
+  );
+}
+
+function AccountNoAccess() {
+  return (
+    <Center height="100vh">
+      <Box
+        border="1px solid rgba(0,0,0,.05)"
+        p={30}
+        borderRadius={10}
+        maxW={500}
+      >
+        <Text>
+          You don't have access to this dashboard. If you think this is an error
+          please contact the web administrators.
+        </Text>
+        <Box height={4} />
+        <Button width="100%" onClick={(e) => getAuth().signOut()}>
+          Logout
+        </Button>
+      </Box>
+    </Center>
+  );
+}
+
+function LoginBox() {
+  const toast = useToast();
+
+  return (
+    <Center height="100vh">
+      <Box
+        border="1px solid rgba(0,0,0,.05)"
+        p={30}
+        borderRadius={10}
+        maxW={500}
+      >
+        <Text>
+          This is the admin dashboard for the institution. Only authorized
+          personnel have access.
+        </Text>
+        <Text marginTop={4} display="flex" alignItems="center">
+          LOGIN WITH
+          <Box width={2} />
+          <FcGoogle size={30} />
+        </Text>
         <Button
+          w="100%"
+          marginTop={5}
           onClick={(e) => {
             const provider = new GoogleAuthProvider();
             signInWithPopup(getAuth(), provider)
               .then((result) => {
                 // The signed-in user info.
                 const user = result.user;
-                toast({
-                  title: "Success!.",
-                  description: "Signed in as: " + user.email,
-                  status: "success",
-                  duration: 9000,
-                  isClosable: true,
-                });
+                if (user.uid === MyTexts.SERVER_ACCOUNT_UID) {
+                  toast({
+                    title: "Success!.",
+                    description: "Signed in as: " + user.email,
+                    status: "success",
+                    duration: 9000,
+                    isClosable: true,
+                  });
+                }
               })
               .catch((error) => {
                 console.error(error);
@@ -52,11 +121,7 @@ export default function DashboardPage() {
         >
           Login with Google
         </Button>
-      )}
-    </div>
+      </Box>
+    </Center>
   );
-}
-
-function SideNavBar() {
-  return <></>;
 }
